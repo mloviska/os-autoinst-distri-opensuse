e2a3cae436a8bba7a44260767bb51ba8d8defdc6
Martin Loviska <mloviska@suse.com>
2019-01-18 14:55:21 +0100

    Adjust iscsi MM test according to  bsc#1115648

--- tests/iscsi/iscsi_client.pm
+++ tests/iscsi/iscsi_client.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2016-2018 SUSE LLC
+# Copyright © 2016-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -15,48 +15,132 @@ use base "x11test";
 use strict;
 use testapi;
 use mm_network;
+use mm_tests 'configure_static_network';
 use lockapi;
-use utils 'turn_off_gnome_screensaver';
+use utils qw(turn_off_gnome_screensaver systemctl);
+use version_utils qw(is_sle is_leap);
+use yast2_widget_utils 'change_service_configuration';
+use y2logsstep;
 
-sub run {
+my %initiator_conf = (
+    ip                 => '10.0.2.3/24',
+    lun                => '/root/iscsi-disk',
+    acl_target_user    => 'test_target',
+    acl_initiator_user => 'test_initiator',
+    acl_pass           => 'susetesting',
+    name               => 'iqn.2016-02.de.openqa',
+    target_ip          => '10.0.2.1'
+);
+
+sub prepare_xterm {
     x11_start_program('xterm -geometry 160x45+5+5', target_match => 'xterm');
     turn_off_gnome_screensaver;
     become_root;
-    configure_default_gateway;
-    configure_static_ip('10.0.2.3/24');
-    configure_static_dns(get_host_resolv_conf());
-    type_string "yast2 iscsi-client\n";
+}
+
+sub initiator_service_tab {
     assert_screen 'iscsi-client', 180;
-    mutex_lock('iscsi_ready');    # wait for server setup
-    send_key "alt-i";             # go to initiator name field
+    unless (is_sle('<15') || is_leap('<15.1')) {
+        change_service_configuration(
+            after_writing => {start         => 'alt-f'},
+            after_reboot  => {start_on_boot => 'alt-a'}
+        );
+    }
+    send_key "alt-i";    # go to initiator name field
     wait_still_screen(2, 10);
-    type_string "iqn.2016-02.de.openqa";
+    type_string $initiator_conf{name};
     wait_still_screen(2, 10);
     assert_screen 'iscsi-initiator-service';
-    send_key "alt-v";             # go to discovered targets tab
+}
+
+sub initiator_discovered_targets_tab {
+    send_key "alt-v";    # go to discovered targets tab
     assert_screen 'iscsi-discovered-targets', 120;
-    send_key "alt-d";             # press discovery button
+    send_key "alt-d";    # press discovery button
     assert_screen 'iscsi-discovery';
-    send_key "alt-i";             # go to IP address field
+    send_key "alt-i";    # go to IP address field
     wait_still_screen(2, 10);
-    type_string "10.0.2.1";
+    type_string $initiator_conf{target_ip};
     assert_screen 'iscsi-initiator-discovered-IP-adress';
     send_key "alt-n";                                     # next
     assert_and_click 'iscsi-initiator-connect-button';    # press connect button
     assert_screen 'iscsi-initiator-connect-manual';
-    send_key "alt-n";                                     # go to connected targets tab
+    send_key 'alt-o';
+    assert_screen 'iscsi-initiator-discovery-enable-login-auth';
+    send_key 'alt-u';
+    type_string $initiator_conf{acl_initiator_user};
+    assert_screen 'iscsi-initiator-discovery-auth-initiators-username';
+    send_key 'alt-p';
+    my $init_pass = reverse $initiator_conf{acl_pass};
+    wait_screen_change { type_string $init_pass; };
+    send_key 'alt-r';
+    type_string $initiator_conf{acl_target_user};
+    assert_screen 'iscsi-initiator-discovery-auth-targets-username';
+    send_key 'alt-a';
+    wait_screen_change { type_string $initiator_conf{acl_pass}; };
+    send_key 'alt-n';
+}
+
+sub initiator_connected_targets_tab {
+    send_key "alt-d";    # go to discovered targets tab
     assert_screen 'iscsi-initiator-discovered-targets';
-    send_key "alt-n";                                     # next
+    send_key "alt-n";    # go to connected targets tab
     assert_screen 'iscsi-initiator-connected-targets';
-    send_key "alt-o";                                     # OK
-    wait_still_screen(2, 10);
+    send_key "alt-o";    # OK
+}
+
+
+sub run {
+    prepare_xterm;
+    record_info 'Network', 'Configure MM network - client';
+    configure_static_network($initiator_conf{ip});
+    mutex_wait('iscsi_target_ready', undef, 'Target configuration in progress!');
+    record_info 'Target Ready!', 'iSCSI target is configured, start initiator configuration';
+    type_string "yast2 iscsi-client; echo yast2-iscsi-client-\$? > /dev/$serialdev\n";
+    initiator_service_tab;
+    initiator_discovered_targets_tab;
+    initiator_connected_targets_tab;
+    wait_serial("yast2-iscsi-client-0", 180) || die "'yast2 iscsi-client ' didn't finish or exited with non-zero code";
+    # logging in to a target will create a local disc device
+    # it takes a moment, since udev actually handles it
+    sleep 5;
+    record_info 'Systemd', 'Verify status of iscsi services and sockets';
+    systemctl("is-active iscsid.service",    expect_false => 0);
+    systemctl("is-active iscsid.socket",     expect_false => 0);
+    systemctl("is-active iscsi.service",     expect_false => 0);
+    systemctl("is-active iscsiuio.service",  expect_false => 0);
+    systemctl("is-active iscsiuio.socket",   expect_false => 1);
+    systemctl("is-active targetcli.service", expect_false => 1);
+    record_info 'Verify LUN availability';
     assert_script_run 'lsscsi';
+    # later delete, moved to post_fail hook
+    assert_script_run 'iscsiadm --mode node session -P 1';
+    assert_script_run 'ls /dev/disk/by-path';
+    assert_script_run 'lsblk --scsi';
+    # making a single partition actually causes the kernel code to re-read the starting part of the disc
+    # in order for it to recognize that you now have a partition table when before there was none
     assert_script_run "echo -e \"n\\np\\n1\\n\\n\\nw\\n\" \| fdisk /dev/sda";    # create one partition
+    sleep 3;
     assert_script_run 'mkfs.ext4 /dev/sda1';                                     # format partition to ext4
+    sleep 2;
     assert_script_run 'mount /dev/sda1 /mnt';                                    # mount partition to /mnt
     assert_script_run 'echo "iscsi is working" > /mnt/iscsi';                    # write text to file on iscsi disk
     assert_script_run 'grep "iscsi is working" /mnt/iscsi';                      # grep expected text from file
+    mutex_create('iscsi_initiator_ready');
+    mutex_wait('iscsi_display_sessions', undef, 'Verifying sessions on target');
+    record_info 'Logout iSCSI', 'Logout iSCSI sessions & unmount LUN';
+    assert_script_run 'iscsiadm --mode node --logoutall=all';                    # log out from target
+    assert_script_run 'umount /mnt';
     type_string "killall xterm\n";
+}
+
+sub post_fail_hook {
+
+    select_console 'root-console';
+    upload_logs '/var/log/boot.log';
+    assert_script_run 'iptables -A INPUT -p tcp --dport 20000:22000';
+    assert_script_run 'iscsiadm --mode node session -P 1';
+    y2logsstep::save_upload_y2logs;
 }
 
 1;
--- tests/iscsi/iscsi_server.pm
+++ tests/iscsi/iscsi_server.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2016-2018 SUSE LLC
+# Copyright © 2016-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -15,89 +15,215 @@ use base "x11test";
 use strict;
 use testapi;
 use mm_network;
+use mm_tests 'configure_static_network';
 use lockapi;
 use version_utils qw(is_sle is_leap);
 use mmapi;
-use utils qw(zypper_call turn_off_gnome_screensaver);
+use utils qw(zypper_call turn_off_gnome_screensaver systemctl);
 use yast2_widget_utils 'change_service_configuration';
+use y2logsstep;
 
-sub run {
-    my $self = shift;
+my %target_conf = (
+    ip                 => '10.0.2.1/24',
+    lun                => '/root/iscsi-disk',
+    acl_target_user    => 'test_target',
+    acl_initiator_user => 'test_initiator',
+    acl_pass           => 'susetesting',
+    name               => 'iqn.2016-02.de.openqa',
+    id                 => '132'
+);
 
+sub prepare_xterm {
     x11_start_program('xterm -geometry 160x45+5+5', target_match => 'xterm');
     turn_off_gnome_screensaver;
     become_root;
-    configure_default_gateway;
-    configure_static_ip('10.0.2.1/24');
-    configure_static_dns(get_host_resolv_conf());
-    zypper_call 'in yast2-iscsi-lio-server';
-    assert_script_run 'dd if=/dev/zero of=/root/iscsi-disk seek=1M bs=8192 count=1';    # create iscsi LUN
-    type_string "yast2 iscsi-lio-server; echo yast2-iscsi-server-\$? > /dev/$serialdev\n";
+}
+
+sub prepare_iscsi_deps {
+    zypper_call 'in yast2-iscsi-lio-server targetcli';
+    assert_script_run 'dd if=/dev/zero of=' . $target_conf{lun} . ' seek=1M bs=8192 count=1';    # create iscsi LUN
+}
+
+sub target_service_tab {
     assert_screen 'iscsi-lio-server';
     unless (is_sle('<15') || is_leap('<15.1')) {
         change_service_configuration(
             after_writing => {start         => 'alt-w'},
             after_reboot  => {start_on_boot => 'alt-a'}
         );
+    } else {
+        send_key 'alt-b';
     }
-    send_key 'alt-o';                                                                   # open port in firewall
+    send_key 'alt-o';    # open port in firewall
     wait_still_screen(2, 10);
     assert_screen 'iscsi-target-overview-service-tab';
-    send_key 'alt-g';                                                                   # go to global tab
-    assert_screen 'iscsi-target-overview-global-tab';
-    send_key 'alt-t';                                                                   # go to target tab
+}
+
+sub config_2way_authentication {
+    assert_screen 'iscsi-target-modify-acls';
+    send_key 'alt-a';
+    assert_screen 'iscsi-target-modify-acls-initiator-popup';
+    if (is_sle('>=15')) {
+        send_key 'alt-i';
+    } else {
+        send_key_until_needlematch 'iscsi-client-name-selected', 'tab';
+    }
+
+    type_string $target_conf{name};
+    send_key 'alt-o';
+    assert_screen 'iscsi-target-modify-acls';
+    send_key 'alt-u';
+    assert_screen 'iscsi-target-modify-acls-authentication';
+    # initiator & target credential fields are swapped in sle12 and sle15
+    my %key_shortcuts;
+    if (is_sle('>=15')) {
+        $key_shortcuts{enable_auth_init}   = 'alt-h';
+        $key_shortcuts{auth_init_user}     = 'alt-m';
+        $key_shortcuts{auth_init_pass}     = 'alt-t';
+        $key_shortcuts{enable_auth_target} = 'alt-n';
+        $key_shortcuts{auth_target_user}   = 'alt-u';
+        $key_shortcuts{auth_target_pass}   = 'alt-p';
+
+    } else {
+        $key_shortcuts{enable_auth_init}   = 'alt-t';
+        $key_shortcuts{auth_init_user}     = 'alt-s';
+        $key_shortcuts{auth_init_pass}     = 'alt-a';
+        $key_shortcuts{enable_auth_target} = 'alt-h';
+        $key_shortcuts{auth_target_user}   = 'alt-u';
+        $key_shortcuts{auth_target_pass}   = 'alt-p';
+    }
+    send_key $key_shortcuts{enable_auth_init};
+    assert_screen 'iscsi-target-acl-auth-initiator-enable-auth';
+    send_key $key_shortcuts{auth_init_user};
+    type_string $target_conf{acl_initiator_user};
+    assert_screen 'iscsi-target-acl-auth-initiator-username';
+    send_key $key_shortcuts{auth_init_pass};
+    my $init_pass = reverse $target_conf{acl_pass};
+    type_string $init_pass;
+    assert_screen 'iscsi-target-acl-auth-initiator-pass' if is_sle('>=15');
+    send_key $key_shortcuts{enable_auth_target};
+    assert_screen 'iscsi-target-acl-auth-target-enable-auth';
+    send_key $key_shortcuts{auth_target_user};
+    type_string $target_conf{acl_target_user};
+    assert_screen 'iscsi-target-acl-auth-target-username';
+    send_key $key_shortcuts{auth_target_pass};
+    type_string $target_conf{acl_pass};
+    assert_screen 'iscsi-target-acl-auth-target-pass' if is_sle('>=15');
+    send_key 'alt-o';
+    assert_screen 'iscsi-target-modify-acls';
+    send_key 'alt-n';
+    if (is_sle('>=15')) {
+        assert_screen 'iscsi-target-acl-warning';
+        send_key 'alt-y';
+    }
+
+}
+
+sub target_backstore_tab {
+    send_key 'alt-t';    # go to target tab
     wait_still_screen(2, 10);
-    send_key 'alt-a';                                                                   # add target
+    send_key 'alt-a';    # add target
     wait_still_screen(2, 10);
-    send_key 'alt-t';                                                                   # select target field
+    send_key 'alt-t';    # select target field
     wait_still_screen(2, 10);
-    send_key 'ctrl-a';                                                                  # select all text inside target field
+    send_key 'ctrl-a';    # select all text inside target field
     wait_still_screen(2, 10);
-    send_key 'delete';                                                                  # text it is automatically selected after tab, delete
-    type_string 'iqn.2016-02.de.openqa';
+    send_key 'delete';    # text it is automatically selected after tab, delete
+    type_string $target_conf{name};
     wait_still_screen(2, 10);
-    send_key 'tab';                                                                     # tab to identifier field
+    send_key 'tab';       # tab to identifier field
     wait_still_screen(2, 10);
     send_key 'delete';
     wait_still_screen(2, 10);
-    type_string '132';
-    wait_still_screen(2, 10);
+    type_string $target_conf{id};
     if (is_sle('>=15')) {
-        send_key 'alt-l';                                                               # un-check bind all IPs
-    }
-    else {
-        send_key 'alt-u';                                                               # un-check use authentication
+        wait_still_screen(1);
+        send_key 'alt-l';    # un-check bind all IPs
+                             # check use authentication only on sle15
+                             # checked in sle12 by default
+        wait_still_screen(1);
+        send_key 'alt-u';
     }
     wait_still_screen(2, 10);
-    send_key 'alt-a';                                                                   # add LUN
+    send_key 'alt-a';        # add LUN
     my $lunpath_key = is_sle('>=15') ? 'alt-l' : 'alt-p';
     send_key_until_needlematch 'iscsi-target-LUN-path-selected', $lunpath_key, 5, 5;    # send $lunpath_key until LUN path is selected
-    type_string '/root/iscsi-disk';
+    type_string $target_conf{lun};
     assert_screen 'iscsi-target-LUN';
     send_key 'alt-o';                                                                   # OK
     assert_screen 'iscsi-target-overview';
     send_key 'alt-n';                                                                   # next
-    wait_still_screen(2, 10);
-    if (is_sle('<15')) {
-        send_key 'alt-a';                                                               # add client
-        send_key_until_needlematch 'iscsi-client-name-selected', 'tab';                 # there is no field shortcut, so tab till client name field is selected
-        type_string 'iqn.2016-02.de.openqa';
-        assert_screen 'iscsi-target-client-name';
-        send_key 'alt-o';                                                               # OK
-        assert_screen 'iscsi-target-client-setup';
-        send_key 'alt-n';                                                               # next
-    }
+    config_2way_authentication;
     assert_screen 'iscsi-target-overview-target-tab';
     send_key 'alt-f';                                                                   # finish
+}
+
+sub display_targets {
+    my (%args) = @_;
+    my $cmd = 'targetcli sessions list';
+    assert_script_run 'targetcli ls';
+    # targetcli does not support sessions option in sle12
+    return if (is_sle '<15');
+    $cmd .= '| grep -i ' . $args{expected} if defined($args{expected});
+    assert_script_run $cmd;
+}
+
+sub run {
+    my $self = shift;
+    # open xterm, configure server network and create drive for iscsi
+    prepare_xterm;
+    record_info 'Network', 'Configure MM network - server';
+    configure_static_network($target_conf{ip});
+    prepare_iscsi_deps;
+    # verify iscsi connection before setup
+    record_info 'iSCSI Sessions', 'Display target sessions & settings before iscsi configuration';
+    display_targets(expected => qq('no open sessions'));
+    # start yast2 wizard
+    record_info 'iSCSI target', 'Start target configuration';
+    type_string "yast2 iscsi-lio-server; echo yast2-iscsi-server-\$? > /dev/$serialdev\n";
+    target_service_tab;
+    target_backstore_tab;
     wait_serial("yast2-iscsi-server-0", 180) || die "'yast2 iscsi-lio ' didn't finish or exited with non-zero code";
-    mutex_create('iscsi_ready');                                                        # setup is done client can connect
+    # verify systemd services after configuration
+    record_info 'Systemd - after', 'Verify status of iscsi services and sockets';
+    my $service = (is_sle('>=15') ? 'targetcli.service' : 'target.service');
+    systemctl("is-active $service", expect_false => 0);
+    # create mutex for child job -> triggers start of initiator configuration
+    # setup is done client can connect
+    mutex_create('iscsi_target_ready');
+    my $children = get_children();
+    my $child_id = (keys %$children)[0];
+    # wait for child mutex, initiator is being configured
+    mutex_wait("iscsi_initiator_ready", $child_id, 'Initiator configuration in progress!');
+    # verify iscsi connections, ACL must be set!
+    record_info 'iSCSI Sessions', 'Display target sessions & settings after setup';
+    display_targets(expected => 'LOGGED_IN');
+    # initiator can continue to test iscsi drive
+    mutex_create('iscsi_display_sessions');
+    # wait idle while initiator finishes its execution
+    wait_for_children;
     type_string "killall xterm\n";
-    wait_for_children;                                                                  # run till client is done
     $self->result('ok');
 }
 
 sub test_flags {
     return {fatal => 1};
+}
+
+sub post_fail_hook {
+
+    select_console 'root-console';
+    display_targets;
+    # show amount of normal logouts
+    assert_script_run 'cat /sys/kernel/config/target/iscsi/iqn.2016-02.de.openqa\\:132/fabric_statistics/iscsi_logout_stats/normal_logouts';
+    # show amount of abnormal logouts
+    assert_script_run 'cat /sys/kernel/config/target/iscsi/iqn.2016-02.de.openqa\\:132/fabric_statistics/iscsi_logout_stats/abnormal_logouts';
+    # show amount of active sessions
+    assert_script_run 'cat /sys/kernel/config/target/iscsi/iqn.2016-02.de.openqa\\:132/fabric_statistics/iscsi_instance/sessions';
+    # show ACL information
+    assert_script_run 'cat /sys/kernel/config/target/iscsi/iqn.2016-02.de.openqa\\:132/tpgt_1/acls/iqn.2016-02.de.openqa/info';
+    y2logsstep::save_upload_y2logs;
+
 }
 
 1;
