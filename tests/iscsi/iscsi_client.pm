8683bb74dfb042240f39919be120cd2178da2768
Martin Loviska <mloviska@suse.com>
2019-01-18 14:55:21 +0100

    Adjust iscsi MM test according to  bsc#1115648

--- tests/iscsi/iscsi_client.pm
+++ tests/iscsi/iscsi_client.pm
@@ -16,28 +16,31 @@ use strict;
 use testapi;
 use mm_network;
 use lockapi;
-use utils 'turn_off_gnome_screensaver';
+use utils qw(turn_off_gnome_screensaver systemctl);
+use y2logsstep;
 
 sub run {
     x11_start_program('xterm -geometry 160x45+5+5', target_match => 'xterm');
     turn_off_gnome_screensaver;
     become_root;
+    record_info 'Network', 'Configure MM network - client';
     configure_default_gateway;
     configure_static_ip('10.0.2.3/24');
     configure_static_dns(get_host_resolv_conf());
+    mutex_wait('iscsi_target_ready');    # wait for server setup
+    record_info 'Target Ready!', 'iSCSI target is configured, start initiator configuration';
     type_string "yast2 iscsi-client\n";
     assert_screen 'iscsi-client', 180;
-    mutex_lock('iscsi_ready');    # wait for server setup
-    send_key "alt-i";             # go to initiator name field
+    send_key "alt-i";                    # go to initiator name field
     wait_still_screen(2, 10);
     type_string "iqn.2016-02.de.openqa";
     wait_still_screen(2, 10);
     assert_screen 'iscsi-initiator-service';
-    send_key "alt-v";             # go to discovered targets tab
+    send_key "alt-v";                    # go to discovered targets tab
     assert_screen 'iscsi-discovered-targets', 120;
-    send_key "alt-d";             # press discovery button
+    send_key "alt-d";                    # press discovery button
     assert_screen 'iscsi-discovery';
-    send_key "alt-i";             # go to IP address field
+    send_key "alt-i";                    # go to IP address field
     wait_still_screen(2, 10);
     type_string "10.0.2.1";
     assert_screen 'iscsi-initiator-discovered-IP-adress';
@@ -49,14 +52,43 @@ sub run {
     send_key "alt-n";                                     # next
     assert_screen 'iscsi-initiator-connected-targets';
     send_key "alt-o";                                     # OK
-    wait_still_screen(2, 10);
+                                                          # logging in to a target will create a local disc device
+                                                          # it takes a moment, since udev actually handles it
+    sleep 5;
+    #wait_still_screen(2, 10);
+    record_info 'Systemd', 'Verify status of iscsi services and sockets';
+    systemctl("is-active iscsid.service",    expect_false => 0);
+    systemctl("is-active iscsid.socket",     expect_false => 0);
+    systemctl("is-active iscsi.service",     expect_false => 0);
+    systemctl("is-active iscsiuio.service",  expect_false => 1);
+    systemctl("is-active iscsiuio.socket",   expect_false => 1);
+    systemctl("is-active targetcli.service", expect_false => 1);
+    record_info 'Verify LUN availability';
     assert_script_run 'lsscsi';
+    assert_script_run 'iscsiadm --mode node session -P 1';
+    assert_script_run 'ls /dev/disk/by-path';
+    assert_script_run 'lsblk --scsi';
+    # making a single partition actually causes the kernel code to re-read the starting part of the disc
+    # in order for it to recognize that you now have a partition table when before there was none
     assert_script_run "echo -e \"n\\np\\n1\\n\\n\\nw\\n\" \| fdisk /dev/sda";    # create one partition
+    sleep 2;
     assert_script_run 'mkfs.ext4 /dev/sda1';                                     # format partition to ext4
+    sleep 2;
     assert_script_run 'mount /dev/sda1 /mnt';                                    # mount partition to /mnt
     assert_script_run 'echo "iscsi is working" > /mnt/iscsi';                    # write text to file on iscsi disk
     assert_script_run 'grep "iscsi is working" /mnt/iscsi';                      # grep expected text from file
+    mutex_create('iscsi_initiator_ready');
+    mutex_wait('iscsi_display_sessions');
+    record_info 'Logout iSCSI', 'Logout iSCSI sessions & unmount LUN';
+    assert_script_run 'iscsiadm --mode node --logoutall=all';                    # log out from target
+    assert_script_run 'umount /mnt';
     type_string "killall xterm\n";
+}
+
+sub post_fail_hook {
+    my $self = shift;
+
+    $self->save_upload_y2logs;
 }
 
 1;
--- tests/iscsi/iscsi_server.pm
+++ tests/iscsi/iscsi_server.pm
@@ -18,7 +18,7 @@ use mm_network;
 use lockapi;
 use version_utils qw(is_sle is_leap);
 use mmapi;
-use utils qw(zypper_call turn_off_gnome_screensaver);
+use utils qw(zypper_call turn_off_gnome_screensaver systemctl);
 use yast2_widget_utils 'change_service_configuration';
 
 sub run {
@@ -27,11 +27,16 @@ sub run {
     x11_start_program('xterm -geometry 160x45+5+5', target_match => 'xterm');
     turn_off_gnome_screensaver;
     become_root;
+    record_info 'Network', 'Configure MM network - server';
     configure_default_gateway;
     configure_static_ip('10.0.2.1/24');
     configure_static_dns(get_host_resolv_conf());
     zypper_call 'in yast2-iscsi-lio-server';
     assert_script_run 'dd if=/dev/zero of=/root/iscsi-disk seek=1M bs=8192 count=1';    # create iscsi LUN
+    record_info 'iSCSI Sessions', 'Display target sessions & settings';
+    assert_script_run 'targetcli ls';
+    assert_script_run 'targetcli sessions list';
+    record_info 'iSCSI target', 'Start target configuration';
     type_string "yast2 iscsi-lio-server; echo yast2-iscsi-server-\$? > /dev/$serialdev\n";
     assert_screen 'iscsi-lio-server';
     unless (is_sle('<15') || is_leap('<15.1')) {
@@ -40,36 +45,50 @@ sub run {
             after_reboot  => {start_on_boot => 'alt-a'}
         );
     }
-    send_key 'alt-o';                                                                   # open port in firewall
+    send_key 'alt-o';    # open port in firewall
     wait_still_screen(2, 10);
     assert_screen 'iscsi-target-overview-service-tab';
-    send_key 'alt-g';                                                                   # go to global tab
+    send_key 'alt-g';    # go to global tab
     assert_screen 'iscsi-target-overview-global-tab';
-    send_key 'alt-t';                                                                   # go to target tab
+    send_key 'alt-o';    # enable auth
+    assert_screen 'iscsi-target-overview-global-tab-enable-auth';
+    send_key 'alt-n';    #enable auth by targets
+    assert_screen 'iscsi-target-overview-global-tab-enable-auth-targets';
+    send_key 'alt-u';
+    type_string "test_target\n";
+    send_key 'tab';
+    type_string "susetesting\n";
+    send_key 'alt-i';
+    assert_screen 'iscsi-target-overview-global-tab-enable-auth-initiators';
+    send_key 'alt-m';
+    type_string "test_initiator\n";
+    send_key 'tab';
+    type_string "susetesting\n";
+    send_key 'alt-t';    # go to target tab
     wait_still_screen(2, 10);
-    send_key 'alt-a';                                                                   # add target
+    send_key 'alt-a';    # add target
     wait_still_screen(2, 10);
-    send_key 'alt-t';                                                                   # select target field
+    send_key 'alt-t';    # select target field
     wait_still_screen(2, 10);
-    send_key 'ctrl-a';                                                                  # select all text inside target field
+    send_key 'ctrl-a';    # select all text inside target field
     wait_still_screen(2, 10);
-    send_key 'delete';                                                                  # text it is automatically selected after tab, delete
+    send_key 'delete';    # text it is automatically selected after tab, delete
     type_string 'iqn.2016-02.de.openqa';
     wait_still_screen(2, 10);
-    send_key 'tab';                                                                     # tab to identifier field
+    send_key 'tab';       # tab to identifier field
     wait_still_screen(2, 10);
     send_key 'delete';
     wait_still_screen(2, 10);
     type_string '132';
     wait_still_screen(2, 10);
     if (is_sle('>=15')) {
-        send_key 'alt-l';                                                               # un-check bind all IPs
+        send_key 'alt-l';    # un-check bind all IPs
     }
     else {
-        send_key 'alt-u';                                                               # un-check use authentication
+        send_key 'alt-u';    # un-check use authentication
     }
     wait_still_screen(2, 10);
-    send_key 'alt-a';                                                                   # add LUN
+    send_key 'alt-a';        # add LUN
     my $lunpath_key = is_sle('>=15') ? 'alt-l' : 'alt-p';
     send_key_until_needlematch 'iscsi-target-LUN-path-selected', $lunpath_key, 5, 5;    # send $lunpath_key until LUN path is selected
     type_string '/root/iscsi-disk';
@@ -90,9 +109,24 @@ sub run {
     assert_screen 'iscsi-target-overview-target-tab';
     send_key 'alt-f';                                                                   # finish
     wait_serial("yast2-iscsi-server-0", 180) || die "'yast2 iscsi-lio ' didn't finish or exited with non-zero code";
-    mutex_create('iscsi_ready');                                                        # setup is done client can connect
-    type_string "killall xterm\n";
+    record_info 'Systemd - after', 'Verify status of iscsi services and sockets';
+    systemctl("is-active iscsid.service",    expect_false => 1);
+    systemctl("is-active iscsid.socket",     expect_false => 0);
+    systemctl("is-active iscsi.service",     expect_false => 0);
+    systemctl("is-active iscsiuio.service",  expect_false => 1);
+    systemctl("is-active iscsiuio.socket",   expect_false => 1);
+    systemctl("is-active targetcli.service", expect_false => 0);
+
+    mutex_create('iscsi_target_ready');                                                 # setup is done client can connect
+    my $children = get_children();
+    my $child_id = (keys %$children)[0];
+    mutex_wait("iscsi_initiator_ready", $child_id);
+    record_info 'iSCSI Sessions', 'Display target sessions & settings';
+    assert_script_run 'targetcli ls';
+    assert_script_run 'targetcli sessions list';
+    mutex_create('iscsi_display_sessions');
     wait_for_children;                                                                  # run till client is done
+    type_string "killall xterm\n";
     $self->result('ok');
 }
 
